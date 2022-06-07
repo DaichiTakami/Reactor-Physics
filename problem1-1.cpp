cc75fa3438f4a53df8d7e2c1192a7b3fc540b583
takami <daichi0229basketball@eis.hokudai.ac.jp>
2022-06-08 04:53:28 +0900

    [Add] index

---
+++ problem1-1.cpp
@@ -0,0 +1,22 @@
+#include<cmath>
+#include<iostream>
+
+using namespace std ;
+
+int main(){
+
+    double a = 1. ;
+    double b = -1. ;
+    double c = -1.9 ;
+
+    double x = -5 ;
+    for (int i = 1 ; i <= 1000 ; i++){
+        double fx = a*x*x+b*x+c ;
+        if(abs(fx) < 0.015){
+            cout<<x<<"\n" ;
+        };
+    x+=0.01;
+    };
+
+    return 0 ;
+};
