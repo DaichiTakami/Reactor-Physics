ab997743cd3ffdd183904e9679201792ed4f3ebe
DaichiTakami <107075240+DaichiTakami@users.noreply.github.com>
2022-06-09 17:12:26 +0900

    aaa

--- random2.cpp
+++ random2.cpp
@@ -12,12 +12,13 @@ int main(){
 
     srand(time(NULL)) ;
 
-    int a = 0 ;
-    int b = 0 ;
-    int c = 0 ;
-    int d = 0 ;
-    int e = 0 ;
-    int f = 0 ;
+    // int a = 0 ;
+    // int b = 0 ;
+    // int c = 0 ;
+    // int d = 0 ;
+    // int e = 0 ;
+    // int f = 0 ;
+    int a,b,c,d,e,f ;
     int x;
     int max = 10000 ;
 
