d7dc39c9bb996350a6f4a2ed8a15f7367e586473
DaichiTakami <daichi0229basketball@eis.hokudai.ac.jp>
2022-06-24 12:57:44 +0900

    aiueo

---
+++ random4.cpp
@@ -0,0 +1,27 @@
+#include<cmath>
+#include<iostream>
+
+using namespace std ;
+
+// void(){
+
+
+
+    
+// };
+
+int main(){
+
+    srand(10);
+
+    double z_1 = (double)rand()/RAND_MAX ;
+    double z_2 = (double)rand()/RAND_MAX ;
+
+    cout<<z_1<<"\n";
+    cout<<z_2<<"\n";
+
+
+
+    
+    return 0 ;
+};
