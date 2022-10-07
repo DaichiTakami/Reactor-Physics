5db96f7c5e9f1cac015ba24aac35e6fa079e9f24
DaichiTakami <daichi0229basketball@eis.hokudai.ac.jp>
2022-10-07 16:20:50 +0900

    ohisashiburidesu

--- random4.cpp
+++ random4.cpp
@@ -7,23 +7,67 @@ using namespace std ;
 
 const double PI = acos(-1);
 
-void box(double x, double y){
-
-    double z_1 = sqrt(-2*log(x))*cos(2*PI*y); 
-
-    
-};
+const double sigma = 0.55 ;
+const double ave = 5.38 ;
 
 int main(){
 
     srand(10);
 
-    double x = (double)rand()/RAND_MAX ;
-    double y = (double)rand()/RAND_MAX ;
+    int num = 10 ;
 
-    box(x,y);
+    vector<double> a(num) ;
+    double b = 0. ;
+    double v = 0. ;
+    double sk = 0. ;
+    double ku = 0. ;
+
+    int c = 10000 ;
+
+    double range = 6*sigma ;
+
+    double dx = range/c ;
+    
+    for (int i = 0; i < num; i++)
+    {
+        double x = (double)rand()/RAND_MAX ;
+        double y = (double)rand()/RAND_MAX ;
+
+        double w = sqrt(-2*log(x))*cos(2*PI*y);
+        double z = sigma*w + ave ;
+
+        a[i] = z ;
+        b += z ;
+
+        // cout<<z<<"\n";
+
+        
+
+
+        
+    }
+    
+    b = b/num ;
+
+    for (int i = 0; i < num; i++){
+
+        double x = a[i]-b ;
+        
+        v += x*x/(num - 1) ;
+        sk += (x/sigma)*(x/sigma)*(x/sigma) *num/((num-1)*(num-2)) ;
+        ku += (x/sigma)*(x/sigma)*(x/sigma)*(x/sigma) * num * (num+1)/((num-1)*(num-2)*(num-3));
+
+    }
+
+    ku = ku - 3*(num-1)*(num-1)/((num-2)*(num-3)) ;
+
+
+
 
     
+    
+
+        
 
 
     // cout<<z_1<<"\n";
